3b2a01191f19120ac8d6aeedbf03cc8cb88c5da2
ffuxa <ffuxa@umich.edu>
2018-03-26 00:09:46 -0400

    Added link to groove bio Winter 2018

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 import SudokuSolver from './containers/SudokuSolver/SudokuSolver';
 import Home from './containers/Home/Home';
 import SoulSacrificeCastingCall from './containers/SoulSacrificeCastingCall/SoulSacrificeCastingCall';
+import RickAstley from './containers/RickAstley/RickAstley';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/sudoku" component={SudokuSolver} />
         <Route path="/soulsacrifice" component={SoulSacrificeCastingCall} />
+        <Route path="/a_complete_guide_to_human_norms_and_culture_vol3" component={RickAstley} />
       </div>
     </Router>
   );
---
+++ src/containers/RickAstley/RickAstley.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class RickAstley extends Component {
+  componentDidMount() {
+    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }
+
+  render() {
+    return (
+      <div>Loading...</div>
+    )
+  }
+}
+
+export default RickAstley;
