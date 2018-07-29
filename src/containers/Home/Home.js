f1789febba14df08f8cecbc57e3f8decda36ef79
ffuxa <ffuxa@umich.edu>
2018-07-29 13:24:40 -0500

    Updated bootstrap version, resume and link style

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
   return (
     <div>
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
-            <a className="navbar-brand" href="/">Fabian Fuxa</a>
-          </div>
-          <div id="navbar" className="collapse navbar-collapse">
-            <ul className="nav navbar-nav">
-              <li><a href="#about">About</a></li>
-              <li><a href="#projects">Projects</a></li>
-              <li><a href="#resume">Resume</a></li>
-            </ul>
-          </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="https://ffuxa.github.io">Fabian Fuxa</a>
+        <button
+          className="navbar-toggler" type="button" data-toggle="collapse"
+          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+          aria-expanded="false" aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#projects">Projects</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#resume">Resume</a>
+            </li>
+          </ul>
         </div>
       </nav>
 
@@ -30,27 +33,42 @@ function Home() {
       {/* <!--Intro
       –––––––––––––––––––––––––––––––––––––––––––––––––– --> */}
       <div id="intro">
-        <div className="container">
-          <img className="img" src={require('./images/profile.jpg')} alt="" />
-          <h1>Fabian Fuxa</h1>
-          <p className="lead">Aspiring full stack web developer, striving to create elegant solutions to complicated problems</p>
+        <img id="profile-img" src={require('./images/profile.jpg')} alt="" />
+        <br />
 
-          <a href="https://www.linkedin.com/in/fabian-fuxa-215910b8/" rel="noopener noreferrer" target="_blank" data-toggle="tooltip" data-placement="center" title="linkedin.com/in/fabianfuxa/">
-            <i className="fa fa-linkedin fa-lg icon-margin icon" />
-          </a>
+        <h1>Fabian Fuxa</h1>
+        <p className="lead">
+          Aspiring software developer, striving to create elegant solutions to complicated problems
+        </p>
 
-          <a href="mailto:ffuxa@umich.edu" rel="noopener noreferrer" target="_blank" data-toggle="tooltip" data-placement="center" title="ffuxa@umich.edu">
-            <i className="fa fa-envelope fa-lg icon-margin icon" />
-          </a>
+        <a
+          href="https://www.linkedin.com/in/fabian-fuxa-215910b8/"
+          rel="noopener noreferrer" target="_blank" data-toggle="tooltip"
+          data-placement="center" title="linkedin.com/in/fabianfuxa/"
+        >
+          <i className="fa fa-linkedin fa-lg icon-margin icon" />
+        </a>
 
-          <a href="https://github.com/ffuxa" rel="noopener noreferrer" target="_blank" data-toggle="tooltip" data-placement="center" title="github.com/ffuxa">
-            <i className="fa fa-github-alt fa-lg icon-margin icon" />
-          </a>
+        <a
+          href="mailto:ffuxa@umich.edu" rel="noopener noreferrer" target="_blank"
+          data-toggle="tooltip" data-placement="center" title="ffuxa@umich.edu"
+        >
+          <i className="fa fa-envelope fa-lg icon-margin icon" />
+        </a>
 
-          <a href={require('./resume/Resume_Fabian_Fuxa.pdf')} rel="noopener noreferrer" target="_blank" data-toggle="tooltip" data-placement="center" title="Resume">
-            <i className="fa fa-file-text fa-lg icon-margin icon" />
-          </a>
-        </div>
+        <a
+          href="https://github.com/ffuxa" rel="noopener noreferrer" target="_blank"
+          data-toggle="tooltip" data-placement="center" title="github.com/ffuxa"
+        >
+          <i className="fa fa-github-alt fa-lg icon-margin icon" />
+        </a>
+
+        <a
+          href={require('./resume/Resume_Fabian_Fuxa.pdf')} download="Fabian_Fuxa_Resume.pdf"
+          role="button" data-toggle="tooltip" data-placement="center" title="Resume"
+        >
+          <i className="fa fa-file-text fa-lg icon-margin icon" />
+        </a>
       </div>
 
 
@@ -69,72 +87,97 @@ function Home() {
           <br />
 
           <p className="center-text">Hi! Welcome to my page. </p>
-          <p className="center-text">I am a student at the
-            {'University of Michigan studying Computer Science Engineering, and I like to code I guess.'}
+          <p className="center-text">
+            I am a student at the University of Michigan studying Computer Science Engineering,
+            and I like to code I guess
           </p>
           <br />
 
+          <b>Life goals:</b>
           <ul>
-            <b>Life goals:</b>
-            <ul>
-              <li>Get a job as a full stack web developer</li>
-              <li>Become a full stack web developer</li>
-              <li>Make some money</li>
-              <li>Retire and live as a wise old man in the beaches of Puerto Rico</li>
-            </ul>
+            <li>Get a job!</li>
+            <li>Travel!</li>
+            <li>Make some money!</li>
+            <li>Retire and live as a wise old man in the beaches of Puerto Rico</li>
+          </ul>
+          <br />
 
-            <br />
+          <b>Interests:</b>
+          <ul>
+            <li>Mobile and Web Development</li>
+            <li>Distributed Systems</li>
+            <li>Design and Architecture</li>
+            <li>Playing drums/guitar/any instrument I can get my hands on</li>
+            <li>Cooking</li>
+          </ul>
 
-            <b>Interests:</b>
-            <ul>
-              <li>Full Stack Development</li>
-              <li>{'Hacking (unfortunately, not the type of "hacking" from the movies)'}</li>
-              <li>Playing drums/guitar/any instrument I can get my hands on</li>
-              <li>Cooking</li>
-            </ul>
+          <br />
+          <b>Programing Skills</b>
+          <br />
+          <br />
 
-            <br />
-            <b>Programing Skills</b>
-            <br />
-            <br />
-
+          <div className="container">
             <div className="row">
-              <div id="small-img" className="col-xs-12 col-sm-12 col-md-12 col-lg-12 center">
-                <ul>
-                  <li className="list-item">
-                    <img src={require('./images/python.png')} className="img-responsive inline-block list-item-image" alt="" />
-                  </li>
-                  <li className="list-item">
-                    <img src={require('./images/cpp.png')} className="img-responsive inline-block list-item-image" alt="" />
-                  </li>
-                  <li className="list-item">
-                    <img src={require('./images/html_css.png')} className="img-responsive inline-block list-item-image" alt="" />
-                  </li>
-                  <li className="list-item">
-                    <img src={require('./images/js.png')} className="img-responsive inline-block list-item-image" alt="" />
-                  </li>
-                  <li className="list-item">
-                    <img src={require('./images/sql.png')} className="img-responsive inline-block list-item-image" alt="" />
-                  </li>
-                  <li className="list-item">
-                    <img src={require('./images/Android_Robot_100.png')} id="android-icon" className="img-responsive inline-block list-item-image" alt="" />
-                  </li>
-                  <li className="list-item">
-                    <img src={require('./images/swift.png')} className="img-responsive inline-block list-item-image" alt="" />
-                  </li>
-                  <li className="list-item">
-                    <img src={require('./images/react.png')} id="react-icon" className="img-responsive inline-block list-item-image" alt="" />
-                  </li>
-                  <li className="list-item">
-                    <img src={require('./images/django-logo.png')} id="django-icon" className="img-responsive inline-block list-item-image" alt="" />
-                  </li>
-                </ul>
+              <div className="col-sm">
+                <img
+                  src={require('./images/python.png')}
+                  className="img-responsive inline-block list-item-image" alt=""
+                />
+              </div>
+              <div className="col-sm">
+                <img
+                  src={require('./images/cpp.png')}
+                  className="img-responsive inline-block list-item-image" alt=""
+                />
+              </div>
+              <div className="col-sm">
+                <img
+                  src={require('./images/html_css.png')}
+                  className="img-responsive inline-block list-item-image" alt=""
+                />
               </div>
             </div>
-
-            <br />
-
-          </ul>
+            <div className="row">
+              <div className="col-sm">
+                <img
+                  src={require('./images/js.png')}
+                  className="img-responsive inline-block list-item-image" alt=""
+                />
+              </div>
+              <div className="col-sm">
+                <img
+                  src={require('./images/sql.png')}
+                  className="img-responsive inline-block list-item-image" alt=""
+                />
+              </div>
+              <div className="col-sm">
+                <img
+                  src={require('./images/Android_Robot_100.png')}
+                  className="img-responsive inline-block list-item-image" alt=""
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm">
+                <img
+                  src={require('./images/swift.png')}
+                  className="img-responsive inline-block list-item-image" alt=""
+                />
+              </div>
+              <div className="col-sm">
+                <img
+                  src={require('./images/react.png')}
+                  className="img-responsive inline-block list-item-image" alt=""
+                />
+              </div>
+              <div className="col-sm">
+                <img
+                  src={require('./images/django-logo.png')}
+                  className="img-responsive inline-block list-item-image" alt=""
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -143,34 +186,26 @@ function Home() {
       –––––––––––––––––––––––––––––––––––––––––––––––––– --> */}
       <div id="projects">
         <div className="container">
+          <br />
           <h2 className="center-text">Projects</h2>
-
           <br />
 
-          <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <div className="hovereffect">
-              <img className="img-responsive" src={require('./images/profile.jpg')} alt="" />
-              <div className="overlay">
-                <h2>ffuxa.github.io</h2>
-                <a className="info" href="https://ffuxa.github.io">Personal website designed to display bio, contact info, and personal projects (this one!). Built on React.js</a>
+          <div id="project-images">
+            <a className="project-image-link" href="https://ffuxa.github.io">
+              <img src={require('./images/profile.jpg')} alt="ffuxa.github.io" className="project-image" />
+              <div className="project-image-middle">
+                <div className="project-image-middle-text">ffuxa.github.io</div>
               </div>
-            </div>
-          </div>
-
-          <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <Link to="/sudoku">
-              <div className="hovereffect">
-                <img className="img-responsive" src={require('./images/sudoku.png')} alt="" />
-                <div className="overlay">
-                  <h2>Sudoku Solver</h2>
-                  <a className="info">High speed sudoku solver that solves any sudoku puzzle in seconds</a>
-                </div>
+            </a>
+            <a className="project-image-link" href="https://ffuxa.github.io/sudoku">
+              <img src={require('./images/sudoku.png')} alt="sudoku" className="project-image" />
+              <div className="project-image-middle">
+                <div className="project-image-middle-text">Sudoku Solver</div>
               </div>
-            </Link>
+            </a>
           </div>
 
           <br />
-
         </div>
       </div>
 
@@ -182,29 +217,40 @@ function Home() {
           <h2 className="center-text">Resume</h2>
 
           <br />
-          <a href={require('./resume/Resume_Fabian_Fuxa.pdf')} target="_blank">
+          <a href={require('./resume/Resume_Fabian_Fuxa.pdf')} download="Fabian_Fuxa_Resume.pdf" role="button">
             <img id="resumeJPG" src={require('./images/resume.png')} alt="" />
           </a>
           <br />
-
           <br />
-          <div id="download">
-            <a className="btn btn-primary" href={require('./resume/Resume_Fabian_Fuxa.pdf')} download="resume" role="button">Download</a>
-          </div>
-          <br />
-
         </div>
       </div>
 
 
       {/* <!-- Footer
       –––––––––––––––––––––––––––––––––––––––––––––––––– --> */}
-      <nav className="navbar navbar-default navbar-bottom">
-        <div className="container">
-          <ul className="nav navbar-nav">
-            <li><a href="https://www.linkedin.com/in/fabian-fuxa-215910b8/" rel="noopener noreferrer" target="_blank">LinkedIn</a></li>
-            <li><a href="mailto:ffuxa@umich.edu" rel="noopener noreferrer" target="_blank">Email</a></li>
-            <li><a href="https://github.com/ffuxa" rel="noopener noreferrer" target="_blank">GitHub</a></li>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          className="navbar-toggler" type="button" data-toggle="collapse"
+          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+          aria-expanded="false" aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="https://www.linkedin.com/in/fabian-fuxa-215910b8/"
+                 rel="noopener noreferrer" target="_blank">LinkedIn</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link"
+                 href="mailto:ffuxa@umich.edu" rel="noopener noreferrer" target="_blank">Email</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link"
+                 href="https://github.com/ffuxa" rel="noopener noreferrer" target="_blank">GitHub</a>
+            </li>
           </ul>
         </div>
       </nav>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 // import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
